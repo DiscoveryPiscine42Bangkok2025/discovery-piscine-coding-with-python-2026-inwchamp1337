2c2b2f172964fc330ec1be634ff92f0e7d6fde35
inwchamp1337 <ppasinb@gmail.com>
2026-02-11 21:11:24 +0700

    Create isneg.py

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+num = float(input(""))
+if num > 0:
+    print("This number is positive.")
+elif num < 0:
+    print("This number is negative.")
+else:
+    print("This number is equal to zero.")
